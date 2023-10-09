21c324871b418bad32d73a2c8925d8590ff2b421
kubrasayitass <145451189+kubrasayitass@users.noreply.github.com>
2023-10-09 11:36:42 +0300

    Add files via upload

---
+++ bolum1odev3/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bolum1odev3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string kullanici_adi;
+            kullanici_adi = textBox1.Text;
+            if (kullanici_adi == "milliegitim@meb.k12.tr")
+            {
+            MessageBox.Show("kullanıcı sisteme kayıtlıdır.");
+            }
+            else
+            {
+                MessageBox.Show("kullanıcı adınız yanlıştır.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
